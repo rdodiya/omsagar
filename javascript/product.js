31d3c9c6b0d5feb4f7e41efe2bcfa0fdc9251026
Raj Dodiya <rdodiya@github.com>
2021-03-31 18:26:44 +0600

    change & update grey_products.html ,add product.css ,product.js file & link index.html/contact in navigation

---
+++ javascript/product.js
@@ -0,0 +1,583 @@
+function toggleText()
+{ 
+
+  
+
+            // Get all the elements from the page 
+
+            var points =  
+
+                document.getElementById("points"); 
+
+  
+
+            var showMoreText = 
+
+                document.getElementById("moreText"); 
+
+            // If the display property of the dots  
+
+            // to be displayed is already set to  
+
+            // 'none' (that is hidden) then this  
+
+            // section of code triggers 
+
+            if (points.style.display === "none") { 
+
+  
+
+                // Hide the text between the span 
+
+                // elements 
+
+                showMoreText.style.display = "none"; 
+
+  
+
+                // Show the dots after the text 
+
+                points.style.display = "inline"; 
+
+
+            } 
+
+  
+
+            // If the hidden portion is revealed, 
+
+            // we will change it back to be hidden 
+
+            else { 
+
+  
+
+                // Show the text between the 
+
+                // span elements 
+
+                showMoreText.style.display = "inline"; 
+
+  
+
+                // Hide the dots after the text 
+
+                points.style.display = "none"; 
+
+
+            } 
+
+} 
+//---------------------------------------------------------------------------------------------
+function toggleText2()
+{ 
+
+  
+
+            // Get all the elements from the page 
+
+            var points =  
+
+                document.getElementById("points2"); 
+
+  
+
+            var showMoreText = 
+
+                document.getElementById("moreText2"); 
+
+            // If the display property of the dots  
+
+            // to be displayed is already set to  
+
+            // 'none' (that is hidden) then this  
+
+            // section of code triggers 
+
+            if (points.style.display === "none") { 
+
+  
+
+                // Hide the text between the span 
+
+                // elements 
+
+                showMoreText.style.display = "none"; 
+
+  
+
+                // Show the dots after the text 
+
+                points.style.display = "inline"; 
+
+
+            } 
+
+  
+
+            // If the hidden portion is revealed, 
+
+            // we will change it back to be hidden 
+
+            else { 
+
+  
+
+                // Show the text between the 
+
+                // span elements 
+
+                showMoreText.style.display = "inline"; 
+
+  
+
+                // Hide the dots after the text 
+
+                points.style.display = "none"; 
+
+
+            } 
+
+} 
+
+//---------------------------------------------------------------------------------------------
+function toggleText3()
+{ 
+
+  
+
+            // Get all the elements from the page 
+
+            var points =  
+
+                document.getElementById("points3"); 
+
+  
+
+            var showMoreText = 
+
+                document.getElementById("moreText3"); 
+
+            // If the display property of the dots  
+
+            // to be displayed is already set to  
+
+            // 'none' (that is hidden) then this  
+
+            // section of code triggers 
+
+            if (points.style.display === "none") { 
+
+  
+
+                // Hide the text between the span 
+
+                // elements 
+
+                showMoreText.style.display = "none"; 
+
+  
+
+                // Show the dots after the text 
+
+                points.style.display = "inline"; 
+
+
+            } 
+
+  
+
+            // If the hidden portion is revealed, 
+
+            // we will change it back to be hidden 
+
+            else { 
+
+  
+
+                // Show the text between the 
+
+                // span elements 
+
+                showMoreText.style.display = "inline"; 
+
+  
+
+                // Hide the dots after the text 
+
+                points.style.display = "none"; 
+
+
+            } 
+
+} 
+//---------------------------------------------------------------------------------------------
+function toggleText4()
+{ 
+
+  
+
+            // Get all the elements from the page 
+
+            var points =  
+
+                document.getElementById("points4"); 
+
+  
+
+            var showMoreText = 
+
+                document.getElementById("moreText4"); 
+
+            // If the display property of the dots  
+
+            // to be displayed is already set to  
+
+            // 'none' (that is hidden) then this  
+
+            // section of code triggers 
+
+            if (points.style.display === "none") { 
+
+  
+
+                // Hide the text between the span 
+
+                // elements 
+
+                showMoreText.style.display = "none"; 
+
+  
+
+                // Show the dots after the text 
+
+                points.style.display = "inline"; 
+
+
+            } 
+
+  
+
+            // If the hidden portion is revealed, 
+
+            // we will change it back to be hidden 
+
+            else { 
+
+  
+
+                // Show the text between the 
+
+                // span elements 
+
+                showMoreText.style.display = "inline"; 
+
+  
+
+                // Hide the dots after the text 
+
+                points.style.display = "none"; 
+
+
+            } 
+
+} 
+
+
+//---------------------------------------------------------------------------------------------
+function toggleText5()
+{ 
+
+  
+
+            // Get all the elements from the page 
+
+            var points =  
+
+                document.getElementById("points5"); 
+
+  
+
+            var showMoreText = 
+
+                document.getElementById("moreText5"); 
+
+            // If the display property of the dots  
+
+            // to be displayed is already set to  
+
+            // 'none' (that is hidden) then this  
+
+            // section of code triggers 
+
+            if (points.style.display === "none") { 
+
+  
+
+                // Hide the text between the span 
+
+                // elements 
+
+                showMoreText.style.display = "none"; 
+
+  
+
+                // Show the dots after the text 
+
+                points.style.display = "inline"; 
+
+
+            } 
+
+  
+
+            // If the hidden portion is revealed, 
+
+            // we will change it back to be hidden 
+
+            else { 
+
+  
+
+                // Show the text between the 
+
+                // span elements 
+
+                showMoreText.style.display = "inline"; 
+
+  
+
+                // Hide the dots after the text 
+
+                points.style.display = "none"; 
+
+
+            } 
+
+} 
+//------------------------------------------
+function toggleText6()
+{ 
+
+  
+
+            // Get all the elements from the page 
+
+            var points =  
+
+                document.getElementById("points6"); 
+
+  
+
+            var showMoreText = 
+
+                document.getElementById("moreText6"); 
+
+            // If the display property of the dots  
+
+            // to be displayed is already set to  
+
+            // 'none' (that is hidden) then this  
+
+            // section of code triggers 
+
+            if (points.style.display === "none") { 
+
+  
+
+                // Hide the text between the span 
+
+                // elements 
+
+                showMoreText.style.display = "none"; 
+
+  
+
+                // Show the dots after the text 
+
+                points.style.display = "inline"; 
+
+
+            } 
+
+  
+
+            // If the hidden portion is revealed, 
+
+            // we will change it back to be hidden 
+
+            else { 
+
+  
+
+                // Show the text between the 
+
+                // span elements 
+
+                showMoreText.style.display = "inline"; 
+
+  
+
+                // Hide the dots after the text 
+
+                points.style.display = "none"; 
+
+
+            } 
+
+} //------------------------------------------------
+function toggleText7()
+{ 
+
+  
+
+            // Get all the elements from the page 
+
+            var points =  
+
+                document.getElementById("points7"); 
+
+  
+
+            var showMoreText = 
+
+                document.getElementById("moreText7"); 
+
+            // If the display property of the dots  
+
+            // to be displayed is already set to  
+
+            // 'none' (that is hidden) then this  
+
+            // section of code triggers 
+
+            if (points.style.display === "none") { 
+
+  
+
+                // Hide the text between the span 
+
+                // elements 
+
+                showMoreText.style.display = "none"; 
+
+  
+
+                // Show the dots after the text 
+
+                points.style.display = "inline"; 
+
+
+            } 
+
+  
+
+            // If the hidden portion is revealed, 
+
+            // we will change it back to be hidden 
+
+            else { 
+
+  
+
+                // Show the text between the 
+
+                // span elements 
+
+                showMoreText.style.display = "inline"; 
+
+  
+
+                // Hide the dots after the text 
+
+                points.style.display = "none"; 
+
+
+            } 
+
+} 
+//-----------------------------------------------------------
+function toggleText8()
+{ 
+
+  
+
+            // Get all the elements from the page 
+
+            var points =  
+
+                document.getElementById("points8"); 
+
+  
+
+            var showMoreText = 
+
+                document.getElementById("moreText8"); 
+
+            // If the display property of the dots  
+
+            // to be displayed is already set to  
+
+            // 'none' (that is hidden) then this  
+
+            // section of code triggers 
+
+            if (points.style.display === "none") { 
+
+  
+
+                // Hide the text between the span 
+
+                // elements 
+
+                showMoreText.style.display = "none"; 
+
+  
+
+                // Show the dots after the text 
+
+                points.style.display = "inline"; 
+
+
+            } 
+
+  
+
+            // If the hidden portion is revealed, 
+
+            // we will change it back to be hidden 
+
+            else { 
+
+  
+
+                // Show the text between the 
+
+                // span elements 
+
+                showMoreText.style.display = "inline"; 
+
+  
+
+                // Hide the dots after the text 
+
+                points.style.display = "none"; 
+
+
+            } 
+
+} 
+//---------------------------------------------------------------
+function changeImage()
+{
+            var img = document.getElementById("image");
+            if(img.src=="img/top_arrow.png")
+            {
+              img.src="img/down_arrow.png";
+            }
+            else
+            {
+              img.src="img/top_arrow.png";
+            }
+}
+//-------------------------------------------------------------------------
