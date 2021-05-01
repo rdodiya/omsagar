32d14c5fe2dd9b6e539a16f170d37368afe5e9d5
Raj Dodiya <rdodiya2001@gmail.com>
2021-05-01 11:07:15 +0600

    Add validation in contact,complain,feedback,cart details form

--- javascript/cart.js
+++ javascript/cart.js
@@ -1,3 +1,29 @@
+
+// Prevent Submission without filling required details
+(function () {
+  'use strict';
+  window.addEventListener('load', function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+          form.addClass("is-invalid");
+        }
+        else {
+         
+          $("#submit-btn").addClass("active");
+          event.sendMail();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
 // ************************************************
 // Shopping Cart API
 // ************************************************
--- javascript/complain.js
+++ javascript/complain.js
@@ -1,26 +1,53 @@
+// Prevent Submission without filling required details
+(function () {
+  'use strict';
+  window.addEventListener('load', function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+          form.addClass("is-invalid");
+        }
+        else {
+         
+          $("#submit-btn").addClass("active");
+       Message1();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
 
-    // Get the modal
-    var modal = document.getElementById("myModal");
+
+  // Get the modal
+  var modal = document.getElementById("myModal");
     
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-    
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-    
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
-    
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
+  // Get the button that opens the modal
+  var btn = document.getElementById("modal-submit");
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  
+  // When the user clicks the button, open the modal 
+  
+  btn.onclick =function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
       modal.style.display = "none";
     }
-    
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
+  }
+
